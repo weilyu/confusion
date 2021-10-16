d902b493ed22814b911fdf21a6ff593542f2175a
Wei Lyu <lvwei1001@live.com>
2021-10-16 22:49:52 +0900

    extract dish detail

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
 import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
+    Card, CardImg, CardImgOverlay, CardTitle
 } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
 
@@ -19,28 +18,11 @@ class Menu extends Component {
         this.setState({ selectedDish: dish });
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
-
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}
+                <div className="col-12 col-md-5 m-1" key={dish.id}>
+                    <Card
                         onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
@@ -56,11 +38,7 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                <DishDetail dish={this.state.selectedDish} />
             </div>
         );
     }
