285067a9f6b9068848d53574d63e9c6920486f5e
Wei Lyu <lvwei1001@live.com>
2021-10-24 20:04:41 +0900

    use redux

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import React, { Component } from 'react';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
@@ -16,11 +20,13 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
